7f10c4b66c2ddd2bc53180cc636f922adb9df294
AugustoGalarza99 <augustogalarza15@gmail.com>
2024-10-16 17:09:09 -0300

    Prueba am-pm

--- src/components/ReservaForm/ReservasForm.jsx
+++ src/components/ReservaForm/ReservasForm.jsx
@@ -115,7 +115,7 @@ const ReservasForm = () => {
                                 let endTime = new Date(`1970-01-01T${endHour1}:00`);
     
                                 while (startTime < endTime) {
-                                    const slotTime = startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+                                    const slotTime = startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
                                     availableSlots.push(slotTime);
                                     startTime.setMinutes(startTime.getMinutes() + 30); // Incrementar 30 minutos
                                 }
