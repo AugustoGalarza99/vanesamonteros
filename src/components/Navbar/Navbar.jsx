0cb35d7a5526132dad4bf5a47470f8cf4ea5094b
AugustoGalarza99 <augustogalarza15@gmail.com>
2024-12-27 11:56:22 -0300

    Actualizacion vanesamonteros

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navbar.css'; // Agregaremos los estilos luego
 import { Link } from 'react-router-dom'; // Necesario cuando configures rutas
-import logo from '../../../public/tulogoaquifooter.png'
+import logo from '../../../public/vanesamonteros.png'
 
 
 function Navbar() { 
@@ -13,7 +13,7 @@ function Navbar() {
       <Link to="/"><img src={logo} alt="Logo"/></Link>
       </div>
       <div className='div-titulo'>
-        <h1 className='titulo-h1'>"Tu marca""</h1>
+        <h1 className='titulo-h1'>Monteros Vanesa <br />Espacio </h1>
       </div>
     </nav>
   );
