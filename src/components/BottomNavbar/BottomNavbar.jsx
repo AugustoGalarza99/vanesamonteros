011dc7c6746ec9f5f82b999480c52068b801cc61
AugustoGalarza99 <augustogalarza15@gmail.com>
2025-01-13 22:21:38 -0300

    Actualizacion vanesa 4.0

--- src/components/BottomNavbar/BottomNavbar.jsx
+++ src/components/BottomNavbar/BottomNavbar.jsx
@@ -1,77 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, db } from '../../firebaseConfig';
-import { getDoc, doc } from 'firebase/firestore';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useRole } from '../../RoleContext'; // Importar el contexto de roles
+import { auth } from '../../firebaseConfig';
 import './BottomNavbar.css';
-import { FiHome, FiUser, FiMoreVertical, FiInfo, FiClock, FiXCircle, FiClipboard, FiBriefcase, FiPieChart, FiShoppingCart, FiTool } from 'react-icons/fi';
-import { RxCalendar } from "react-icons/rx";
+import { FiHome, FiUser, FiMoreVertical, FiInfo, FiClock, FiXCircle, FiClipboard, FiBriefcase, FiPieChart, FiShoppingCart, FiTool,} from 'react-icons/fi';
+import { RxCalendar } from 'react-icons/rx';
 
-function BottomNavbar({ isPeluquero }) {
-  const [user] = useAuthState(auth);
+function BottomNavbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { user, role } = useRole(); // Obtener el usuario y el rol desde el contexto
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const checkIfPeluquero = async (user) => {
-    if (user) {
-      const userRef = doc(db, 'users', user.uid);
-      const userDoc = await getDoc(userRef);
-      if (userDoc.exists() && userDoc.data().rol === 'peluquero') {
-        isPeluquero(true);
-      }
-    }
+  const handleLogout = () => {
+    auth.signOut().then(() => {
+      console.log('Sesión cerrada.');
+      setIsMenuOpen(false);
+    });
   };
-
-  useEffect(() => {
-    if (user) {
-      checkIfPeluquero(user);
-    }
-  }, [user]);
 
   return (
     <>
       <nav className="bottom-navbar">
-        <Link to="/" className="nav-icon">
-          <FiHome size={24} /> Reserva
-        </Link>
-        <Link to="/estado" className="nav-icon">
-        <FiInfo size={24} /> Estado
-        </Link>
-        <Link to="/productos" className='nav-icon'> 
-        <FiShoppingCart size={24} /> Productos
+        <Link to="/" className="nav-icon"><FiHome size={24} /> Reserva</Link>
+        <Link to="/estado" className="nav-icon"><FiInfo size={24} /> Estado</Link>
+        <Link to="/productos" className="nav-icon"><FiShoppingCart size={24} /> Productos
         </Link>
         {user ? (
           <div className="nav-icon" onClick={toggleMenu}>
-            <FiMoreVertical size={24} /> Mas
-          </div>
+            <FiMoreVertical size={24} /> Más</div>
         ) : (
-          <Link to="/login" className="nav-icon">
-            <FiUser size={24} /> Login
-          </Link>
+          <Link to="/login" className="nav-icon"><FiUser size={24} /> Login</Link>
         )}
       </nav>
 
       {isMenuOpen && (
         <div className="dropdown-menu">
           <ul>
-            <li><Link to="/" onClick={() => setIsMenuOpen(false)}> <RxCalendar size={16} /> Reservar turno</Link></li>
-            <li><Link to="/estado" onClick={() => setIsMenuOpen(false)}> <FiInfo size={16} /> Estado</Link></li>
-            {isPeluquero && (
+            <li><Link to="/" onClick={() => setIsMenuOpen(false)}><RxCalendar size={16} /> Reservar turno</Link></li>
+            <li><Link to="/estado" onClick={() => setIsMenuOpen(false)}><FiInfo size={16} /> Estado</Link></li>
+            {role === 'peluquero' && (
               <>
-                <li><Link to="/agenda" onClick={() => setIsMenuOpen(false)}> <FiClipboard size={16} /> Agenda 1</Link></li>
-                <li><Link to="/agendados" onClick={() => setIsMenuOpen(false)}> <FiClipboard size={16} /> Agenda 2</Link></li>
-                <li><Link to="/servicios" onClick={() => setIsMenuOpen(false)}> <FiBriefcase size={16} /> Servicios</Link></li>
-                <li><Link to="/horarios" onClick={() => setIsMenuOpen(false)}> <FiClock size={16} /> Horarios</Link></li>
-                <li><Link to="/reservamanual" onClick={() => setIsMenuOpen(false)}> <RxCalendar size={16} /> Reserva manual</Link></li>
-                <li><Link to="/finanzas" onClick={() => setIsMenuOpen(false)}> <FiPieChart size={16} /> Finanzas</Link></li>
-                <li><Link to="/administracion" onClick={() => setIsMenuOpen(false)}> <FiUser size={16} /> Administracion</Link></li>
-                <li><Link to="/gestionreservas" onClick={() => setIsMenuOpen(false)}> <FiTool size={16} /> Gestion reservas</Link></li>
+                <li><Link to="/agenda" onClick={() => setIsMenuOpen(false)}><FiClipboard size={16} /> Agenda 1</Link></li>
+                <li><Link to="/agendados" onClick={() => setIsMenuOpen(false)}><FiClipboard size={16} /> Agenda 2</Link></li>
+                <li><Link to="/servicios" onClick={() => setIsMenuOpen(false)}><FiBriefcase size={16} /> Servicios</Link></li>
+                <li><Link to="/horarios" onClick={() => setIsMenuOpen(false)}><FiClock size={16} /> Horarios</Link></li>
+                <li><Link to="/reservamanual" onClick={() => setIsMenuOpen(false)}><RxCalendar size={16} /> Reserva manual</Link></li>
+                <li><Link to="/finanzas" onClick={() => setIsMenuOpen(false)}><FiPieChart size={16} /> Finanzas</Link></li>
+                <li><Link to="/administracion" onClick={() => setIsMenuOpen(false)}><FiUser size={16} /> Administración</Link></li>
               </>
             )}
-            <li><a onClick={() => { auth.signOut(); setIsMenuOpen(false); }}> <FiXCircle size={16} /> Cerrar Sesión</a></li>
+            {role === 'administrador' && (
+              <>
+                <li><Link to="/reservamanual" onClick={() => setIsMenuOpen(false)}><RxCalendar size={16} /> Reserva manual</Link></li>
+                <li><Link to="/finanzas" onClick={() => setIsMenuOpen(false)}><FiPieChart size={16} /> Finanzas</Link></li>
+                <li><Link to="/administracion" onClick={() => setIsMenuOpen(false)}><FiUser size={16} /> Administración</Link></li>
+                <li><Link to="/gestionreservas" onClick={() => setIsMenuOpen(false)}><FiTool size={16} /> Gestión Reservas</Link></li>
+              </>
+            )}
+            <li><a onClick={handleLogout}><FiXCircle size={16} /> Cerrar Sesión</a></li>
           </ul>
         </div>
       )}
--- src/components/DesktopNavbar/DesktopNavbar.jsx
+++ src/components/DesktopNavbar/DesktopNavbar.jsx
@@ -1,38 +1,30 @@
-import React, { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, db } from '../../firebaseConfig';
-import { getDoc, doc } from 'firebase/firestore';
+import React from 'react';
+import { useRole } from '../../RoleContext';
+import { auth } from '../../firebaseConfig';
 import { Link } from 'react-router-dom';
 import './DesktopNavbar.css';
 import { FiHome, FiUser, FiInfo, FiClock, FiXCircle, FiClipboard, FiBriefcase, FiPieChart, FiShoppingCart, FiTool } from 'react-icons/fi';
 import { RxCalendar } from "react-icons/rx";
+import Loader from '../Loader/Loader';
 
-function DesktopNavbar({ isPeluquero }) {
-  const [user] = useAuthState(auth);
+function DesktopNavbar() {
+  const { user, role, loading } = useRole();
 
-  const checkIfPeluquero = async (user) => {
-    if (user) {
-      const userRef = doc(db, 'users', user.uid);
-      const userDoc = await getDoc(userRef);
-      if (userDoc.exists() && userDoc.data().rol === 'peluquero') {
-        isPeluquero(true);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (user) {
-      checkIfPeluquero(user);
-    }
-  }, [user]);
+  if (loading) {
+    // Mostrar un indicador de carga si el rol aún se está determinando
+    return <nav className="desktop-navbar"><Loader/></nav>;
+  }
 
   return (
     <nav className="desktop-navbar">
       <ul>
+        {/* Enlaces visibles para todos */}
         <li><Link to="/"><FiHome size={24} /> Inicio</Link></li>
         <li><Link to="/estado"><FiInfo size={24} /> Estado</Link></li>
         <li><Link to="/productos"><FiShoppingCart size={24} /> Productos</Link></li>
-        {isPeluquero && (
+
+        {/* Enlaces para peluqueros */}
+        {role === 'peluquero' && (
           <>
             <li><Link to="/agenda"><FiClipboard size={24} /> Agenda 1</Link></li>
             <li><Link to="/agendados"><FiClipboard size={24} /> Agenda 2</Link></li>
@@ -40,16 +32,37 @@ function DesktopNavbar({ isPeluquero }) {
             <li><Link to="/horarios"><FiClock size={24} /> Horarios</Link></li>
             <li><Link to="/reservamanual"><RxCalendar size={24} /> Reserva manual</Link></li>
             <li><Link to="/finanzas"><FiPieChart size={24} /> Finanzas</Link></li>
-            <li><Link to="/administracion"><FiUser size={24} /> Administracion</Link></li>
-            <li><Link to="/gestionreservas"><FiTool size={24} /> Gestion Reservas</Link></li>
+            <li><Link to="/administracion"><FiUser size={24} /> Administración</Link></li>
           </>
         )}
+
+        {/* Enlaces para administradores */}
+        {role === 'administrador' && (
+          <>
+            <li><Link to="/reservamanual"><RxCalendar size={24} /> Reserva manual</Link></li>
+            <li><Link to="/finanzas"><FiPieChart size={24} /> Finanzas</Link></li>
+            <li><Link to="/administracion"><FiUser size={24} /> Administración</Link></li>
+            <li><Link to="/gestionreservas"><FiTool size={24} /> Gestión Reservas</Link></li>
+          </>
+        )}
+
+        {/* Botón de inicio de sesión o cierre de sesión */}
         {user ? (
           <li>
-            <a onClick={() => auth.signOut()}>
-              <FiXCircle size={24} /> Cerrar Sesión
-            </a>
-          </li>
+          <a
+            onClick={() => {
+              auth.signOut()
+                .then(() => {
+                  console.log('Sesión cerrada exitosamente.');
+                })
+                .catch((error) => {
+                  console.error('Error al cerrar sesión:', error);
+                });
+            }}
+          >
+            <FiXCircle size={24} /> Cerrar Sesión
+          </a>
+        </li>        
         ) : (
           <li><Link to="/login"><FiUser size={24} /> Iniciar Sesión</Link></li>
         )}
@@ -57,6 +70,5 @@ function DesktopNavbar({ isPeluquero }) {
     </nav>
   );
 }
-
 
 export default DesktopNavbar;
